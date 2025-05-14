2e87027b6992669d01c3747e7cc0ef3c1a28ae6e
Br0nken <danilmarakasov1@gmail.com>
2025-05-14 17:27:59 +0300

    12

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+// Настройка хоста с DI
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddTransient<ILoggerService, LoggerService>(); // Регистрация сервиса
+        services.AddTransient<App>(); // Главный класс приложения
+    })
+    .Build();
+
+// Запуск
+var app = host.Services.GetRequiredService<App>();
+app.Run();
+
+// Класс приложения
+public class App
+{
+    private readonly ILoggerService _logger;
+
+    // Внедрение зависимости через конструктор
+    public App(ILoggerService logger)
+    {
+        _logger = logger;
+    }
+
+    public void Run()
+    {
+        _logger.Log("Приложение запущено!");
+        Console.WriteLine("Hello from ConsoleDIApp!");
+        _logger.Log("Приложение завершает работу.");
+    }
+}
